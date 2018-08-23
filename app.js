520ac6a30288c05871a4a4375a1771103de5fe21
marcus koh <kohchihao@gmail.com>
2018-08-23 13:25:07 +0800

    Added comments and code cleanup

--- app.js
+++ app.js
@@ -1,4 +1,4 @@
-require('dotenv').config()
+require('dotenv').config();
 const fs = require('fs');
 const tough = require('tough-cookie');
 const request = require('request-promise');
@@ -8,43 +8,47 @@ const uuidv4 = require('uuid/v4');
 const { Signale } = require('signale');
 const del = require('del');
 
-const options = {
+const logOptions = {
   types: {
     debug: {
       color: 'blue',
       label: 'debug'
-    },
+    }
   }
 };
-const log = new Signale(options);
+const log = new Signale(logOptions);
 
 bot.on('document', ctx => {
   if (
     ctx.message.document &&
     ctx.message.document.mime_type === 'application/pdf'
   ) {
-    
     console.log(ctx.message.document);
     let document = ctx.message.document;
     bot.telegram.getFileLink(document.file_id).then(data => {
-      //console.log('file', data);
-      log.debug('File URL',data);
-      
-      var options = {
+
+      log.debug('File URL', data);
+
+      let options = {
         method: 'GET',
         uri: data,
         resolveWithFullResponse: true
       };
+
       let uuid = uuidv4();
+
+      //create temp folder for pdf and pptx
       if (!fs.existsSync('./temp/' + uuid)) {
         fs.mkdirSync('./temp/' + uuid);
       }
+
+      //make request to download pdf file
       request(options)
         .pipe(fs.createWriteStream('./temp/' + uuid + '/' + document.file_name))
         .on('finish', () => {
           //console.log('finish downloading pdf..');
           log.success('finish downloading pdf...');
-          download('./temp/' + uuid, document.file_name, ctx);
+          convert('./temp/' + uuid, document.file_name, ctx);
         })
         .on('error', error => {
           console.log('Error in creating map', error);
@@ -54,7 +58,7 @@ bot.on('document', ctx => {
   }
 });
 
-const download = (path, fileName, ctx) => {
+const convert = (path, fileName, ctx) => {
   request({
     method: 'POST',
     uri: 'https://simplypdf.com/api/convert',
@@ -68,82 +72,87 @@ const download = (path, fileName, ctx) => {
     },
     resolveWithFullResponse: true
   })
-  .then(r => {
-    console.log('response', r.body);
-    ctx.reply('Converting your pdf.')
-    let id = JSON.parse(r.body).id;
-    let cookieValue = r.headers['set-cookie'][0]
-      .split('=', 2)[1]
-      .slice(0, 24);
-    let cookie = new tough.Cookie({
-      key: 'ASP.NET_SessionId',
-      value: cookieValue,
-      domain: 'simplypdf.com',
-      httpOnly: true
-    });
-    let cookiejar = request.jar();
-    cookiejar.setCookie(cookie, 'https://simplypdf.com');
-    let options = {
-      method: 'GET',
-      uri: 'https://simplypdf.com/api/status/' + id,
-      headers: {
-        Accept: 'application/json'
-      },
-      jar: cookiejar,
-      resolveWithFullResponse: true
-    };
+    .then(r => {
+      console.log('response', r.body);
+      ctx.reply('Converting your pdf.');
 
-    checkStatusAndDownload(ctx,options,cookiejar,path,fileName);
-      
-  })
-  .catch(err => {
-    console.log('err', err);
-  });
-  
+      let id = JSON.parse(r.body).id;
+      let cookieValue = r.headers['set-cookie'][0]
+        .split('=', 2)[1]
+        .slice(0, 24);
+      let cookie = new tough.Cookie({
+        key: 'ASP.NET_SessionId',
+        value: cookieValue,
+        domain: 'simplypdf.com',
+        httpOnly: true
+      });
+      let cookiejar = request.jar();
+      cookiejar.setCookie(cookie, 'https://simplypdf.com');
+      let options = {
+        method: 'GET',
+        uri: 'https://simplypdf.com/api/status/' + id,
+        headers: {
+          Accept: 'application/json'
+        },
+        jar: cookiejar,
+        resolveWithFullResponse: true
+      };
+
+      checkStatusAndDownload(ctx, options, cookiejar, path, fileName);
+    })
+    .catch(err => {
+      log.err('error @ converting', err);
+      ctx.reply('There was an error uploading your file. Please try again.');
+    });
 };
 
-const checkStatusAndDownload = (ctx,options,cookiejar,path,fileName) => {
-  request(options)
-      .then(res => {
-        console.log('status', res.body);
-        let status = JSON.parse(res.body).status;
-        setTimeout(() => {
-          if (status === 'ready') {
-            log.success('ppt is ready for download...')
-            // console.log('ready for download...');
-            let id = JSON.parse(res.body).id;
-            let options = {
-              method: 'GET',
-              uri: 'https://simplypdf.com/api/download/' + id,
-              jar: cookiejar,
-              resolveWithFullResponse: true
-            };
+const checkStatusAndDownload = (ctx, options, cookiejar, path, fileName) => {
+  request(options).then(res => {
+    console.log('status', res.body);
+    let status = JSON.parse(res.body).status;
+    setTimeout(() => {
+      if (status === 'ready') {
+        log.success('ppt is ready for download...');
+        // console.log('ready for download...');
+        let id = JSON.parse(res.body).id;
+        let options = {
+          method: 'GET',
+          uri: 'https://simplypdf.com/api/download/' + id,
+          jar: cookiejar,
+          resolveWithFullResponse: true
+        };
 
-            let file_name = fileName.split('.')[0];
-            request(options)
-              .pipe(fs.createWriteStream(path + '/' + file_name + '.pptx'))
-              .on('finish', function() {
-                log.success('finish downloading pptx...');
-                ctx.reply('finish downloading pptx.');
-                ctx.replyWithDocument({ 
-                  source: path+'/'+file_name + '.pptx',
-                  filename: file_name + '.pptx'
-                }).then(() => {
-                  del([path]).then(paths => {
-                    log.debug('Deleted files and folders:\n', paths.join('\n'));
-                  });
-                })
+        let file_name = fileName.split('.')[0];
+        request(options)
+          .pipe(fs.createWriteStream(path + '/' + file_name + '.pptx'))
+          .on('finish', () => {
+            log.success('finish downloading pptx...');
+            ctx.reply('finish downloading pptx.');
+            ctx.replyWithDocument({
+                source: path + '/' + file_name + '.pptx',
+                filename: file_name + '.pptx'
               })
-              .on('error', function(error) {
-                console.log('Error in creating map', error);
+              .then(() => {
+                del([path]).then(paths => {
+                  log.debug('Deleted files and folders:\n', paths.join('\n'));
+                });
               });
-          } else {
-            log.error('ppt not ready');
-            checkStatusAndDownload(ctx,options,cookiejar,path,fileName);
-          }
-        }, 5000);
-      });
-    
-}
+          })
+          .on('error', function(error) {
+            console.log('Error in creating map', error);
+          });
+      } else {
+        log.error('ppt not ready');
+        checkStatusAndDownload(ctx, options, cookiejar, path, fileName);
+      }
+    }, 5000);
+  });
+};
+
+bot.command('start', ctx => {
+  ctx.reply(
+    'I convert PDF to PPTX. Simply drag and drop your PDF file 📎 and magically a PPTX will be sent to you.'
+  );
+});
 
 bot.startPolling();
